1268bf9c1ac50ad2ace3784d6cc31c0f4fbc1c7e
Shiyam-7 <131951377+Shiyam-7@users.noreply.github.com>
2023-09-29 22:09:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://video-call-project-client.vercel.app/",
-    methods: ["GET", "POST"]
+    origin: "https://video-call-project-client.vercel.app/"
   }
 });
 
