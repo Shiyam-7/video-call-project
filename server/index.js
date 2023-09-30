c5b3ce6dcc2b39ec641d5529f0e08905420e1a26
Shiyam-7 <131951377+Shiyam-7@users.noreply.github.com>
2023-09-30 22:35:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["*"],
     handlePreflightRequest: (req, res) => {
     res .writeHead(200,{
     "Access-Control-Allow-Origin" :"*",
