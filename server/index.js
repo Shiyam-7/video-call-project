f4ea017831d2d5135408d6f94148f39204656799
Shiyam-7 <131951377+Shiyam-7@users.noreply.github.com>
2023-09-30 12:49:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,8 +9,9 @@ const io = new Server(server, {
   }
 });
 
-io.listen(3000, () => {
-  console.log('listening on *:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log('listening on *:port');
 });
 
 app.get('/', (req, res) => {
